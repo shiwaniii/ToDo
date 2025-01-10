ae6ed42cb02d3b9e063ed154edf107674623a25d
shiwaniii <Shiwani.adhikari@gmail.com>
2025-01-10 10:26:54 +0545

    CRUD Operation

--- TODO/urls.py
+++ TODO/urls.py
@@ -21,4 +21,7 @@ from todo_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.todo_list),
+    path("delete/<int:id>/", views.todo_delete),
+    path("create/", views.todo_create),
+    path("update/<int:id>/", views.todo_update),
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -10,3 +10,24 @@ def todo_delete(request, id):
     todo = Todo.objects.get(id=id)
     todo.delete()
     return HttpResponseRedirect("/")
+
+def todo_create(request):
+    if request.method == "GET":
+        return render(request, "todo_create.html")
+    else:
+        title= request.POST["title"]
+        todo =Todo (title=title)
+        todo.save()
+        return HttpResponseRedirect("/")
+    
+  
+    
+ 
+        def todo_update(request, id):
+            todo = Todo.objects.get(id=id)
+            if request.method == "GET":
+                return render(request, "todo_update.html", {"todo": todo},)
+            else:
+                todo.title = request.POST["title"]
+                todo.save()
+                return HttpResponseRedirect("/")
