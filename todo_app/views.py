a284b98b8099d6369bdfd095cc56293b049bc5ef
shiwaniii <Shiwani.adhikari@gmail.com>
2025-01-10 09:49:17 +0545

    deleted data

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from todo_app.models import Todo
 
@@ -5,3 +6,7 @@ def todo_list(request):
     todos= Todo.objects.all()
     return render(request, "todo_list.html", {"todos": todos})
 
+def todo_delete(request, id):
+    todo = Todo.objects.get(id=id)
+    todo.delete()
+    return HttpResponseRedirect("/")
