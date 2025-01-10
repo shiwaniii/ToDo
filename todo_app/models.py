57ffa5c270887b73bc24baddea4ca055486b3d1b
shiwaniii <Shiwani.adhikari@gmail.com>
2025-01-10 09:39:28 +0545

    django

--- todo_app/models.py
+++ todo_app/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class Todo(models.Model):
     title= models.CharField(max_length=200)
 
+    def __str__ (self):
+        return self.title
+
 #When there is change in models.py
 #python manage.py makemigrations
 #python manage.py migrate
+
